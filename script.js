56e5b9144f3e395ae017a37e43b44b8037dea06a
0xnazrul <160778418+Alex000115@users.noreply.github.com>
2026-07-18 21:15:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const typing = document.getElementById("typing");
 const startBtn = document.getElementById("startBtn");
 
-const text = "I Have Something Special To Tell You ❤️";
+const text = "তোমার জন্য আমার একটি বিশেষ কথা আছে... ❤️";
 
 let i = 0;
 
